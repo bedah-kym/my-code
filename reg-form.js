3c374945312ea5fea2691a050ed3804722894fac
bedah <bedankimani860@gmail.com>
2021-06-14 09:07:27 +0300

    hae this is a test run

---
+++ reg-form.js
@@ -0,0 +1,22 @@
+<html>
+<head>
+<body>
+<script type="text/javascript">
+ var images=src=["https://www.pexels.com/photo/black-flat-screen-computer-monitor-1714208/",
+ "https://www.pexels.com/photo/photo-of-keyboard-near-mouse-3184460/",
+ "https://www.pexels.com/photo/photo-of-people-doing-handshakes-3183197/"];
+ function slide(){
+   var slider=document.getElementById("slider");
+  num=0;
+     do{
+       slider.src=images[num];
+     }while num <2;
+     num++;
+ }
+
+
+
+</script>
+</body>
+</head>
+</html>
